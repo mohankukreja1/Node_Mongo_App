0698c9bd1e70896f2857723be197d0276cca7f35
mohan <mohankukreja1@gmail.com>
2019-09-10 17:30:40 +0530

    session initialized

--- routes/index.js
+++ routes/index.js
@@ -4,32 +4,47 @@ var product = require('../models/product');
 var passport = require('passport');
 
 var csrf = require('csurf');
+var Cart = require('../models/cart');
+
 
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var items = product.find(function(err,result){
-    console.log(result);
+    //console.log(result);
     var arr = [];
     for(var i=0;i<result.length;i++){
       var obj = {};
+      obj._id = result[i]._doc._id.toString();
       obj.title = result[i]._doc.title;
       obj.price = result[i]._doc.price;
       obj.imagePath = result[i]._doc.imagePath;
       obj.description = result[i]._doc.description;
+    //  console.log(obj);
       arr.push(obj);
     }
     var finalarr = [];
     for(var i=0 ;i<arr.length;i+=3 ){
       finalarr.push(arr.slice(i,i+3));
     }
-    console.log(finalarr);
+    //console.log(finalarr);
     res.render('shop/index', { title: 'Express', product : finalarr });
   });
   //console.log(items);
 });
 
+router.get('/add-to-cart/:id',function(req,res,next){
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {items : {}, totalQty : 0, totalPrice : 0});
+  product.findById(productId,function(err,item){
+    if(err) throw err;
+    cart.add(item,item.id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect('/');
+  })
+})
 
 
 module.exports = router;
